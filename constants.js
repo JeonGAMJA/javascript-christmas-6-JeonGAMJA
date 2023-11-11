58c759e2342249127542095b3b41b77ab6ed4cec
전소희 <indian_potato@naver.com>
2023-11-12 02:23:58 +0900

    docs: 구현 기능 목록 작성 수정

---
+++ constants.js
@@ -0,0 +1,14 @@
+const MENU = [
+  { menu: '양송이수프', cost: 6000, type: '에피타이저' },
+  { menu: '타파스', cost: 5500, type: '에피타이저' },
+  { menu: '시저샐러드', cost: 8000, type: '에피타이저' },
+  { menu: '티본스테이크', cost: 55000, type: '메인' },
+  { menu: '바비큐립', cost: 54000, type: '메인' },
+  { menu: '해산물파스타', cost: 35000, type: '메인' },
+  { menu: '크리스마스파스타', cost: 25000, type: '메인' },
+  { menu: '초코케이크', cost: 15000, type: '디저트' },
+  { menu: '아이스크림', cost: 5000, type: '디저트' },
+  { menu: '제로콜라', cost: 3000, type: '음료' },
+  { menu: '레드와인', cost: 60000, type: '음료' },
+  { menu: '샴페인', cost: 25000, type: '음료' },
+];
