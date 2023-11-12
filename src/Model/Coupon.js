f58fd4c6eb43e418a5c326f25a18a949c7df7bd3
전소희 <indian_potato@naver.com>
2023-11-12 20:57:40 +0900

    feat:날짜가 1이상 25이하인 경우 크리스마스 디데이 할인 적용

--- src/Model/Coupon.js
+++ src/Model/Coupon.js
@@ -1,2 +1,10 @@
-class Coupon {}
+import InputView from '../View/InputView';
+
+class Coupon {
+  christmasDdayCoupon() {
+    if (day >= 1 && day <= 25) {
+      const christmasDdayCoupon = 1000 + 100 * (day - 1);
+    }
+  }
+}
 export default Coupon;
