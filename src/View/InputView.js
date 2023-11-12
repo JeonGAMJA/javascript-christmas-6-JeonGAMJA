bdb00a7b2faa7ed90a730ebb1e4c44013f5d94c8
전소희 <indian_potato@naver.com>
2023-11-13 00:46:54 +0900

    Refactor:유저에게 메뉴 입력 받기
    메뉴와 메뉴 갯수를 입력받고 오브젝트로 변환하여 출력할 수 있게 수정

--- src/View/InputView.js
+++ src/View/InputView.js
@@ -1,5 +1,6 @@
 import { Console } from '@woowacourse/mission-utils';
 import ErrorHandler from '../ErrorHandler.js';
+import { MENU } from '../constants.js';
 
 const errorHandler = new ErrorHandler();
 
@@ -10,6 +11,7 @@ const InputView = {
         '12월 중 식당 예상 방문 날짜는 언제인가요? (숫자만 입력해 주세요!)',
       );
       errorHandler.validateDay(day);
+      errorHandler.validateNumber(day);
 
       return day;
     } catch (e) {
@@ -19,9 +21,17 @@ const InputView = {
   },
 
   async getMenuUserInput() {
-    const Menu = await Console.readLineAsync(
+    const ordersInput = await Console.readLineAsync(
       `주문하실 메뉴를 메뉴와 개수를 알려 주세요. (e.g. 해산물파스타-2,레드와인-1,초코케이크-1)`,
     );
+
+    const orders = ordersInput.split(',');
+    const order = orders.map(order => order.split('-'));
+    const olderObj = order.reduce((acc, [key, value]) => {
+      acc[key] = Number(value);
+      return acc;
+    }, {});
+    return olderObj;
   },
 };
 
