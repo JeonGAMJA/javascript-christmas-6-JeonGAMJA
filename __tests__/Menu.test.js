350268bdd4fe295a54cd170735f495a9e6ac7a64
전소희 <indian_potato@naver.com>
2023-11-15 23:08:24 +0900

    Test: 메뉴 클래스 테스트(2)
    입력받은 메뉴들의 상세 정보를 반환하는지 검증

--- __tests__/Menu.test.js
+++ __tests__/Menu.test.js
@@ -32,4 +32,27 @@ describe('Menu 클래스 테스트', () => {
 
     expect(result).toEqual([2, 1, 1]);
   });
+
+  test('입력받은 메뉴의 상세정보를 반환한다.', () => {
+    const result = menu.getOrderDetail(mockMenu);
+    const expected = {
+      해산물파스타: {
+        cost: MENU.해산물파스타.cost,
+        quantity: 2,
+        type: MENU.해산물파스타.type,
+      },
+      레드와인: {
+        cost: MENU.레드와인.cost,
+        quantity: 1,
+        type: MENU.레드와인.type,
+      },
+      초코케이크: {
+        cost: MENU.초코케이크.cost,
+        quantity: 1,
+        type: MENU.초코케이크.type,
+      },
+    };
+
+    expect(result).toEqual(expected);
+  });
 });
